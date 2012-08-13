e66aeb827ba1f61d7e2478a3de85a6fdfddda847
amalrik maia <amalrik.maia@gmail.com>
2012-08-12 21:48:35 -0300

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FightBets::Application.config.secret_token = '2fc5c118a01e81aeaee0f29e1ced10e3eeb76bd10bf678f4900228ce707c63960b4345b3426ef2a96156f941ce46aaaf47102194544b84ab94b99c6d6d97d9f5'
