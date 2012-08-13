adc279fc7e24ac9bc189d304b67561a4ca70a001
amalrik maia <amalrik.maia@gmail.com>
2012-08-13 08:38:35 -0300

    mudado banco para postgresql

--- db/migrate/20120813005926_create_fights.rb
+++ db/migrate/20120813005926_create_fights.rb
@@ -1,9 +1,9 @@
 class CreateFights < ActiveRecord::Migration
   def change
     create_table :fights do |t|
-      t.string contender
-      t.string challenger
-      t.integer result_id
+      t.string :contender, :null => false
+      t.string :challenger, :null => false
+      t.integer :result_id
       t.timestamps
     end
   end
