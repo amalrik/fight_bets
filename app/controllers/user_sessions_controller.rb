b56807af385fca9cd10ec7160bf84f7022ebcf84
amalrik maia <amalrik.maia@gmail.com>
2012-08-26 10:56:21 -0300

    pagina de login criada, botoes para aposta adicionados.

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,19 @@
+class UserSessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    if @user = login(params[:username], params[:password])
+      redirect_back_or_to(events_path, :message => 'Logged in successfully.')
+    else  
+      flash.now[:alert] = "Login failed."
+      render :action => :new
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to(:authors, :message => 'Logged out!')
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 FightBets::Application.routes.draw do
   resources :users
 
+  resources :user_sessions
+  match 'login'  => 'user_sessions#new',     :as => :login
+  match 'logout' => 'user_sessions#destroy', :as => :logout
+
   resources :fights
   resources :events
+  resources :bets
   root :to => 'fights#index'
 
 end
