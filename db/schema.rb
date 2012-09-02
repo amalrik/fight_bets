b38a80dbd4130237626a6e7f409395621122bd1e
amalrik maia <amalrik.maia@gmail.com>
2012-09-02 15:19:17 -0300

    reversao no banco de dados, devolvendo fight_id a tabela bets

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120902180403) do
+ActiveRecord::Schema.define(:version => 20120824025638) do
 
   create_table "bets", :force => true do |t|
     t.integer  "user_id"
     t.string   "winner"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "fight_id"
   end
 
   create_table "events", :force => true do |t|
