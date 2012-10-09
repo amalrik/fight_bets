9c2eaf99c1eb2c88d37dff910cdcef87179c173d
amalrik <amalrik.maia@gmail.com>
2012-10-09 02:05:24 -0300

    iniciado criacao do dashboard dos usuarios contendo as apostas atuais realizadas.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
+    @future_bets = @user.bets.select{|b| b.fight.result == nil}
 
     respond_to do |format|
       format.html # show.html.erb
--- db/migrate/20120902180403_remove_fight_id_from_bets.rb
+++
@@ -1,9 +0,0 @@
-class RemoveFightIdFromBets < ActiveRecord::Migration
-  def up
-    remove_column :bets, :fight_id
-  end
-
-  def down
-    add_column :bets, :fight_id, :integer
-  end
-end
